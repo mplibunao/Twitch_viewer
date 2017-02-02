9c90614013c47dfa3b949bad262133c5c3e932a0
Mark Paolo Libunao <Mark Paolo Libunao>
2017-02-03 03:27:38 +0800

    Added initial api call for twitch.tv

--- scripts/script.js
+++ scripts/script.js
@@ -15,13 +15,28 @@ $(document).ready(function(){
 			$('.sidenav-items').hide('fast').css("display", "inline-block");	
 			isClose = true;
 		}
-			//$('.sidenav').animate({width: '+=20%'},'slow');
-		//$(this).animate({width: '=250px'}, 'slow');
-		//$('.sidenav-items').show('slow');
-		
 	});
 
-	
-	//$(this).animate({width: '=250px'}, 'slow');
-		//$('.sidenav-items').show('slow');
+	//load Twitch javascript sdk to perform authentication /login your users for additional permissions
+	//Twitch.init({clientId: 'i0bm039u6j4dr1ifl1t3v2s16srrhq'}, function(error,status){
+		//the sdk is now loaded
+	//	console.log('Twitch javacript sdk now loaded');
+	//});
+
+	checkChannelStatus();
+
+
 })
+
+var checkChannelStatus = function(){
+	$.ajax({
+		type: "GET",
+		url: "https://api.twitch.tv/kraken/streams/ESL_SC2",
+		headers: {
+			'Client-ID': 'i0bm039u6j4dr1ifl1t3v2s16srrhq'
+		},
+		success: function(json){
+			console.log(json);
+		}
+	})
+}
