745d98106318a2cba5ee9fdd248080458b8ec936
Mark Paolo Libunao <Mark Paolo Libunao>
2017-02-02 13:38:06 +0800

    Fixed nav bar css

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
 		if (isClose === true){
 			$('.sidenav').animate({width: '+=250px'}, 'fast');
 			$('.main-container').animate({marginLeft: '+=250px'}, 'slow');
-			$('.sidenav-items').show('slow');			
+			$('.sidenav-items').delay(200).show('fast').css("display", "inline-block");			
 			isClose = false;
 		} else if (isClose === false){
 			$('.sidenav').animate({width: '-=250px'}, 'fast');
 			$('.main-container').animate({marginLeft: '-=250px'}, 'slow');
-			$('.sidenav-items').hide('slow');	
+			$('.sidenav-items').hide('fast').css("display", "inline-block");	
 			isClose = true;
 		}
 			//$('.sidenav').animate({width: '+=20%'},'slow');
